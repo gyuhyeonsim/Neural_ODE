c30780fc81467b193fe0abecc85fa3fe07152e55
comingtoyoulive <ghsim@korea.ac.kr>
2020-11-09 20:06:30 +0900

    upload autonoous GRUODECell

--- models/vanilla_node.py
+++ models/vanilla_node.py
@@ -22,15 +22,47 @@ class NN(nn.Module):
     def forward(self, t, x):
         # t-invariant
         return self.function(x)
-class RNN(nn.Module):
-    def __init__(self, args):
-        super(RNN, self).__init__()
-        self.encoder = nn.Linear(args.model['input_size'], args.model['hidden_size'])
-        self.decoder = nn.Linear(args.model['hidden_size'], args.model['input_size'])
-        self.gru = nn.GRUCell(args.model['input_size'], args.model['hidden_size'])
 
-    def forward(self, t, x):
-        return self.gru(x)
+"""
+https://github.com/edebrouwer/gru_ode_bayes
+"""
+class FullGRUODECell_Autonomous(torch.nn.Module):
+    def __init__(self, hidden_size, bias=True):
+        """
+        For p(t) modelling input_size should be 2x the x size.
+        """
+        super().__init__()
+
+        #self.lin_xh = torch.nn.Linear(input_size, hidden_size, bias=bias)
+        #self.lin_xz = torch.nn.Linear(input_size, hidden_size, bias=bias)
+        #self.lin_xr = torch.nn.Linear(input_size, hidden_size, bias=bias)
+
+        #self.lin_x = torch.nn.Linear(input_size, hidden_size * 3, bias=bias)
+
+        self.lin_hh = torch.nn.Linear(hidden_size, hidden_size, bias=False)
+        self.lin_hz = torch.nn.Linear(hidden_size, hidden_size, bias=False)
+        self.lin_hr = torch.nn.Linear(hidden_size, hidden_size, bias=False)
+
+    def forward(self, t, h):
+        """
+        Executes one step with autonomous GRU-ODE for all h.
+        The step size is given by delta_t.
+
+        Args:
+            t        time of evaluation
+            h        hidden state (current)
+
+        Returns:
+            Updated h
+        """
+        #xr, xz, xh = torch.chunk(self.lin_x(x), 3, dim=1)
+        x = torch.zeros_like(h)
+        r = torch.sigmoid(x + self.lin_hr(h))   # reset gate
+        z = torch.sigmoid(x + self.lin_hz(h))   # zero gate
+        u = torch.tanh(x + self.lin_hh(r * h))  # update gate
+
+        dh = (1 - z) * (u - h)
+        return dh
 
 class NeuralODE(nn.Module):
     def __init__(self, args):
@@ -42,12 +74,14 @@ class NeuralODE(nn.Module):
             self.func = NN(args)
             self.func.to(self.device)
             self.func.double()
+
         elif args.model['ode_func'] == 'rnn':
-            self.func = RNN(args)
+            self.func = FullGRUODECell_Autonomous(args.model['hidden_size'])
             self.encoder = self.func.encoder # linear encoder
             self.decoder = self.func.decoder # linear decoder
 
     # train
     def forward(self, x0, t):
-        pred_y = odeint(self.func, x0, t, method='dopri5').permute(1, 0, 2)
+        if self.args.model['ode_func'] == 'rnn':
+            pred_y = odeint(self.func, x0, t, method='dopri5').permute(1, 0, 2)
         return pred_y
