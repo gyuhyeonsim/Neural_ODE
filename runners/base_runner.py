1b7a83b45113c6e938ce9f9d2872c44dac95398a
root <root@csi-cluster-vgpu9.dakao.io>
2020-11-22 16:31:58 +0900

    amp_test

--- runners/base_runner.py
+++ runners/base_runner.py
@@ -15,7 +15,7 @@ class Runner():
         self.model = model.to(self.torch_device)
         self.optimizer = optimizer
         self.seed = random.randint(0, 10000)
-        self.writer = SummaryWriter("./runs/{}".format(self.args.exid))
+        # self.writer = SummaryWriter("./runs/{}".format(self.args.exid))
         self.metric = dict()
 
         # for debug
@@ -31,15 +31,16 @@ class Runner():
             for j, b in enumerate(self.dl['train']):
                 t = b['t'][0].to(self.torch_device).float()  # all data entries share the same time point
                 point = b['point'].to(self.torch_device).float()
-                freq = b['freq'].unsqueeze(1).float()
-                amp = b['amp'].unsqueeze(1).float()
-                sign = b['sign'].unsqueeze(1).float()
-                latent_v = torch.cat([freq, amp, sign], dim=1).to(self.torch_device)
+                # freq = b['freq'].unsqueeze(1).float()
+                amp = b['amp'].float()
+                print()
+                # sign = b['sign'].unsqueeze(1).float()
+                # latent_v = torch.cat([freq, amp, sign], dim=1).to(self.torch_device)
                 #print(t.size(), point.size(), freq.size(), amp.size())
                 # >> torch.Size([64, 100]) torch.Size([64, 100, 1]) torch.Size([64]) torch.Size([64])
                 # print(torch.cat([freq, amp, sign], dim=1).size())
                 # >> torch.Size([64, 3])
-                pred = self.model(t, point[:,0,:], latent_v=latent_v).permute(1,0,2)
+                pred = self.model(t, point[:,0,:], latent_v=amp).permute(1,0,2)
                 loss = self.cal_loss(pred, point)
                 loss.backward()
                 self.optimizer.step()
@@ -48,12 +49,14 @@ class Runner():
                                                                  j, len(self.dl['train']),
                                                                  loss.item()))
                 iter+=1
-            valid_loss = self.valid(self.dl['valid'])
-            print("[Validation] epoch:{}/{}, iter:{}/{}, loss: {}".format(i,self.args.niter,
-                                                                          iter,len(self.dl['train'])*self.args.niter,
-                                                                          valid_loss.item()))
-            if bset_mse>valid_loss:
-                self.model_save(epoch=i, loss=valid_loss)
+            # valid_loss = self.valid(self.dl['valid'])
+            # print("[Validation] epoch:{}/{}, iter:{}/{}, loss: {}".format(i,self.args.niter,
+            #                                                               iter,len(self.dl['train'])*self.args.niter,
+            #                                                               valid_loss.item()))
+            # if bset_mse>valid_loss:
+            #     self.model_save(epoch=i, loss=valid_loss)
+            if i % 20 == 0:
+                self.model_save(epoch=i, loss=loss)
 
     def valid(self, dataloader):
         self.model.eval()
@@ -77,8 +80,8 @@ class Runner():
 
     def model_save(self, epoch, loss):
         print("saved epoch {}".format(epoch))
-        path = "./save/{}".format(self.args.exid)
-        self.check_direcory('./save')
+        path = "/data/private/generativeODE/galerkin_pretest/{}".format(self.args.exid)
+        self.check_direcory('/data/private/generativeODE/galerkin_pretest')
         self.check_direcory(path)
         torch.save({
             'epoch': epoch,
