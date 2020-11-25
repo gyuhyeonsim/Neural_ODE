9e3a662f2b7c0f1c94448f92279c955913c06fc4
root <jiyounglee0523@gmail.com>
2020-11-25 10:41:50 +0900

    amp_test

--- datasets/sinusoidal_dataloader.py
+++ datasets/sinusoidal_dataloader.py
@@ -5,10 +5,11 @@ import torch
 
 def get_data_loader(args):
     data_loader = dict()
-    train_data = Sinusoid(args.dataset['path'], mode='train')
-    valid_data = Sinusoid(args.dataset['path'], mode='valid')
+    train_data = Sinusoid_from_scratch(mode='train')
+    # train_data = Sinusoid(args.dataset['path'], mode='train')
+    # valid_data = Sinusoid(args.dataset['path'], mode='valid')
     data_loader['train'] = DataLoader(dataset=train_data, batch_size=args.dataset['batch'], shuffle=True)
-    data_loader['valid'] = DataLoader(dataset=valid_data, batch_size=args.dataset['batch'], shuffle=False)
+    # data_loader['valid'] = DataLoader(dataset=valid_data, batch_size=args.dataset['batch'], shuffle=False)
     return data_loader
 
 class Sinusoid(Dataset):
@@ -44,3 +45,66 @@ class Sinusoid(Dataset):
 
     def __len__(self):
         return self.len
+
+
+class Sinusoid_from_scratch(Dataset):
+    def __init__(self, mode='train'):
+        self.mode = mode
+        if mode == 'train':
+            self.samp_sinusoidals, self.samp_ts, self.amps = self.sinusoid_from_scratch(n_sinusoid= 1024, n_total=2000, n_sample=400, skip_step=4)
+        elif mode == 'valid':
+            pass
+        elif mode == 'test':
+            pass
+
+        self.len = self.samp_sinusoidals.size(0)
+        print('[Dataset] Sinusoid_from_scratch dataset for {} is initialized'.format(mode))
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, item):
+        samp_sin = self.samp_sinusoidals[item]
+        samp_ts = self.samp_sinusoidals[item]
+        amp = self.amps[item]
+        dict_to_return = {'point': samp_sin, 't': samp_ts, 'amp':amp}
+        return dict_to_return
+
+
+    def sinusoid_from_scratch(self, n_sinusoid = 1024, n_total=2000, n_sample=400, skip_step=4):
+        start = 0.
+        stop = 6. * np.pi
+        orig_ts = np.linspace(start, stop, num=n_total)
+        samp_ts = orig_ts[0: (n_sample * skip_step): skip_step]
+
+        samp_sinusoidals = []
+        count = n_sinusoid / 4
+        amp1 = [1] * count + [2] * count + [3] * count + [4] * count
+        count = n_sinusoid / 8
+        amp2 = ([1] * count + [2] * count + [3] * count + [4] * count) * 2
+        count = n_sinusoid / 16
+        amp3 = ([1] * count + [2] * count + [3] * count + [4] * count) * 4
+        count = n_sinusoid / 32
+        amp4 = ([1] * count + [2] * count + [3] * count + [4] * count) * 8
+
+        amps = np.stack((amp1, amp2, amp3, amp4), axis=1)
+        assert len(amps) == n_sinusoid, "amp list incorrectly constructed!"
+
+        for i in range(n_sinusoid):
+            amp = amps[i]
+            samp_sinusoidal = -amp[0] * np.cos(samp_ts) -amp[1] * 0.5 * np.cos(2 * samp_ts) + amp[2] * np.sin(samp_ts) + amp[3] * 0.5 * np.sin(2*samp_ts)
+            samp_sinusoidals.append(samp_sinusoidal)
+
+        samp_sinusoidals = np.stack(samp_sinusoidals, axis=0)
+        samp_sinusoidals = torch.unsqueeze(torch.Tensor(samp_sinusoidals), dim=-1)  # batch_size x seq_len x 1
+        amps = torch.Tensor(amps)  # batch_size x 4
+        samp_ts = torch.Tensor([samp_ts] * n_sinusoid)   # n_sinusoid x 400
+        return samp_sinusoidals, samp_ts, amps
+
+
+
+
+
+
+
+
--- models/generative_ode.py
+++ models/generative_ode.py
@@ -23,6 +23,15 @@ def fourier_expansion(n_range, s):
     basis = [torch.cos(s_n_range), torch.sin(s_n_range)]
     return basis
 
+def batch_fourier_expansion(n_range, s):
+    # s is shape of (batch_size x n_eig * n_harmonics)
+    cos_s = s[:, :n_range.size(0)] * n_range
+    cos_s = torch.diag_embed(torch.cos(cos_s))
+    sin_s = s[:, n_range.size(0):] * n_range
+    sin_s = torch.diag_embed(torch.sin(sin_s))
+    return torch.cat((cos_s, sin_s), dim=1)
+
+
 def poly_expansion(n_range, s):
     """Polynomial expansion
     """
@@ -46,45 +55,101 @@ class CoeffDecoder(nn.Module):
         out = self.act2(self.fc2(out))
         return self.fc3(out)
 
+
+# class WeightAdaptiveGallinear(nn.Module):
+#     def __init__(self, hidden_dim, in_features = 1, out_features = 16,
+#                  latent_dimension = 3, expfunc = fourier_expansion, n_harmonics = 5, n_eig = 2, dilation = False, shift = False):
+#         super().__init__()
+#
+#         self.in_features, self.out_features, self.latent_dimension = in_features, out_features, latent_dimension
+#         self.dilation = torch.ones(1) if not dilation else nn.Parameter(data = torch.ones(1), requires_grad=True)
+#         self.shift = torch.zeros(1) if not shift else nn.Parameter(data=torch.zeros(1), requires_grad=True)
+#         self.expfunc = expfunc
+#         self.n_harmonics, self.n_eig = n_harmonics, n_eig
+#
+#         coeffs_size = (in_features + 1) * out_features * n_harmonics * n_eig
+#
+#         # latent_dimension 3 means amp, sign, phase
+#         self.coeffs_generator = CoeffDecoder(latent_dimension, hidden_dim, coeffs_size)
+#
+#     def assign_weights(self, s, coeffs):
+#         n_range = torch.linspace(0, self.n_harmonics, steps=self.n_harmonics).to(self.input.device)
+#         basis = self.expfunc(n_range, s * self.dilation.to(self.input.device) + self.shift.to(self.input.device))
+#         B = []
+#         for i in range(self.n_eig):
+#             Bin = torch.eye(self.n_harmonics).to(self.input.device)
+#             Bin[range(self.n_harmonics), range(self.n_harmonics)] = basis[i]
+#             B.append(Bin)
+#         B = torch.cat(B, 1).permute(1, 0).to(self.input.device)
+#         X = torch.matmul(coeffs, B)
+#         return X.sum(2)
+#
+#     def forward(self, x):
+#         assert x.size(1) == (self.in_features + self.latent_dimension + 1) # x should be ordered in input_data, s, latent_variables
+#         self.batch_size = x.size(0)
+#         s = x[-1, self.in_features]
+#         self.input = torch.unsqueeze(x[:, :self.in_features], dim=-1)
+#         latent_variables = x[:,-self.latent_dimension:]
+#
+#         coeffs = self.coeffs_generator(latent_variables).reshape(self.batch_size, (self.in_features + 1) * self.out_features, self.n_eig * self.n_harmonics)
+#
+#         w = self.assign_weights(s, coeffs)
+#         self.weight = w[:, :(self.in_features * self.out_features)].reshape(self.batch_size, self.in_features, self.out_features)
+#         self.bias = w[:, (self.in_features * self.out_features):((self.in_features + 1) * self.out_features)].reshape(self.batch_size, self.out_features)
+#
+#         self.weighted = torch.squeeze(torch.bmm(self.input, self.weight), dim=1)
+#         return torch.add(self.weighted, self.bias)
+
+'''
+change shift and dilation
+'''
+
 class WeightAdaptiveGallinear(nn.Module):
     def __init__(self, hidden_dim, in_features = 1, out_features = 16,
-                 latent_dimension = 3, expfunc = fourier_expansion, n_harmonics = 5, n_eig = 2, dilation = False, shift = False):
+                 latent_dimension = 3, expfunc = batch_fourier_expansion, n_harmonics = 5, n_eig = 2):
         super().__init__()
 
         self.in_features, self.out_features, self.latent_dimension = in_features, out_features, latent_dimension
-        self.dilation = torch.ones(1) if not dilation else nn.Parameter(data = torch.ones(1), requires_grad=True)
-        self.shift = torch.zeros(1) if not shift else nn.Parameter(data=torch.zeros(1), requires_grad=True)
+        # self.dilation = torch.ones(1) if not dilation else nn.Parameter(data = torch.ones(1), requires_grad=True)
+        # self.shift = torch.zeros(1) if not shift else nn.Parameter(data=torch.zeros(1), requires_grad=True)
         self.expfunc = expfunc
         self.n_harmonics, self.n_eig = n_harmonics, n_eig
 
-        coeffs_size = (in_features + 1) * out_features * n_harmonics * n_eig
+        coeffs_size = ((in_features + 1) * out_features + 2)* n_harmonics * n_eig   # +1 more dilation and shift
 
         # latent_dimension 3 means amp, sign, phase
         self.coeffs_generator = CoeffDecoder(latent_dimension, hidden_dim, coeffs_size)
-        self.depth_cat = DepthCat(1)
 
-    def assign_weights(self, s, coeffs):
-        n_range = torch.linspace(0, self.n_harmonics, steps=self.n_harmonics).to(self.input.device)
-        basis = self.expfunc(n_range, s * self.dilation.to(self.input.device) + self.shift.to(self.input.device))
-        B = []
-        for i in range(self.n_eig):
-            Bin = torch.eye(self.n_harmonics).to(self.input.device)
-            Bin[range(self.n_harmonics), range(self.n_harmonics)] = basis[i]
-            B.append(Bin)
-        B = torch.cat(B, 1).permute(1, 0).to(self.input.device)
-        X = torch.matmul(coeffs, B)
+    def assign_weights(self, s, coeffs, dilation, shift):
+        n_range = torch.Tensor([0., 1., 2.]).to(self.input.device)
+        # n_range = torch.linspace(0, self.n_harmonics, steps=self.n_harmonics).to(self.input.device)
+        s = s.new_full((self.batch_size, self.n_eig * self.n_harmonics), s)
+        s = (s * dilation) + shift
+
+        B = self.expfunc(n_range, s)   ## shape of (batch_size, n_eig * n_harmonics, n_harmonics)
+        # B = []
+        # for i in range(self.n_eig):
+        #     Bin = torch.eye(self.n_harmonics).to(self.input.device)
+        #     Bin[range(self.n_harmonics), range(self.n_harmonics)] = basis[i]
+        #     B.append(Bin)
+        # B = torch.cat(B, 1).permute(1, 0).to(self.input.device)   # shape of (n_harmonics*n_eig, n_harmonics)
+        # B = torch.matmul(torch.unsqueeze(dilation, dim=1), B)
+        X = torch.bmm(coeffs, B)
         return X.sum(2)
 
     def forward(self, x):
         assert x.size(1) == (self.in_features + self.latent_dimension + 1) # x should be ordered in input_data, s, latent_variables
         self.batch_size = x.size(0)
         s = x[-1, self.in_features]
-        self.input = torch.unsqueeze(x[:, :self.in_features], dim=-1)
-        latent_variables = x[:,-self.latent_dimension:]
+        self.input = torch.unsqueeze(x[:, :self.in_features], dim=-1)   # shape of (batch_size x in_feature x 1)
+        latent_variables = x[:,-self.latent_dimension:]  # shape of (batch_size x latent_dim)
 
-        coeffs = self.coeffs_generator(latent_variables).reshape(self.batch_size, (self.in_features + 1) * self.out_features, self.n_eig * self.n_harmonics)
+        coeffs = self.coeffs_generator(latent_variables).reshape(self.batch_size, ((self.in_features + 1) * self.out_features + 2)* self.n_eig * self.n_harmonics)
+        self.coeff = coeffs[:, :((self.in_features + 1) * self.out_features * self.n_eig * self.n_harmonics)].reshape(self.batch_size, (self.in_features + 1) * self.out_features, self.n_eig * self.n_harmonics)
+        self.dilation = coeffs[:, ((self.in_features + 1) * self.out_features * self.n_eig * self.n_harmonics): (((self.in_features + 1) * self.out_features + 1)* self.n_eig * self.n_harmonics)]
+        self.shift = coeffs[:, (((self.in_features + 1) * self.out_features + 1)* self.n_eig * self.n_harmonics):]
 
-        w = self.assign_weights(s, coeffs)
+        w = self.assign_weights(s, self.coeff, self.dilation, self.shift)
         self.weight = w[:, :(self.in_features * self.out_features)].reshape(self.batch_size, self.in_features, self.out_features)
         self.bias = w[:, (self.in_features * self.out_features):((self.in_features + 1) * self.out_features)].reshape(self.batch_size, self.out_features)
 
